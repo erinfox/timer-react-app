668db4d56104f76f410feeaa5e69e79098c29f5b
Erin Fox <erinfox@Erins-MacBook-Pro.local>
2019-07-18 15:46:06 -0700

    display time and buttons

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -38,9 +38,31 @@ class Stopwatch extends Component {
   };
 
   render() {
+    const {timerTime} = this.state;
+
+    let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+
     return (
       <div className="Stopwatch">
         <div className="Stopwatch-header">Stopwatch</div>
+        <div className="Stopwatch-display">
+          {hours} : {minutes} : {seconds} : {centiseconds}
+          {this.state.timerOn === false && this.state.timerTime === 0 && (
+            <button onClick={this.startTimer}>Start</button>
+          )}
+          {this.state.timerOn === true && (
+            <button onClick={this.stopTimer}>Stop</button>
+          )}
+          {this.state.timerOn === false && this.state.timerTime > 0 && (
+            <button onClick={this.startTimer}>Resume</button>
+          )}
+          {this.state.timerOn === false && this.state.timerTime > 0 && (
+            <button onClick={this.resetTimer}>Reset</button>
+          )}
+        </div>
       </div>
     );
   }
